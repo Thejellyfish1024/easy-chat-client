a0012d3b806dae32a0011bb8baabe5bedf21a9de
Thejellyfish1024 <alamin102410@gmail.com>
2024-03-30 06:41:01 +0600

    installad tanstack and made axiosSecure hook with inceptors

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,38 +1,38 @@
-// import axios from "axios";
-// import useAuth from "./useAuth";
-// import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import useAuth from "./useAuth";
+import { useNavigate } from "react-router-dom";
 
-// const axiosSecure = axios.create({
-//     baseURL: 'https://elite-care-server.vercel.app'
-// })
+const axiosSecure = axios.create({
+    baseURL: 'https:localhost:5000'
+})
 
-// const useAxiosSecure = () => {
-//     const {logOut} = useAuth()
-//     const navigate = useNavigate()
+const useAxiosSecure = () => {
+    const {logOut} = useAuth()
+    const navigate = useNavigate()
 
-//     axiosSecure.interceptors.request.use(function (config) {
-//         const token = localStorage.getItem('access-token')
-//         // console.log('request stopped by interceptors', token);
-//         config.headers.authorization = `Bearer ${token}`
-//         return config
-//     }, function (error) {
-//         return Promise.reject(error)
-//     })
+    axiosSecure.interceptors.request.use(function (config) {
+        const token = localStorage.getItem('access-token')
+        // console.log('request stopped by interceptors', token);
+        config.headers.authorization = `Bearer ${token}`
+        return config
+    }, function (error) {
+        return Promise.reject(error)
+    })
 
-//     axiosSecure.interceptors.response.use(function (response) {
-//         return response;
-//     },  async (error) => {
-//         const status = error?.response?.status;
-//         // console.log('status', status);
-//         if(status === 401 || status === 403){
-//             // console.log('asi');
-//             await logOut();
-//             navigate('/login')
-//         }
-//         return Promise.reject(error);
-//     });
+    axiosSecure.interceptors.response.use(function (response) {
+        return response;
+    },  async (error) => {
+        const status = error?.response?.status;
+        // console.log('status', status);
+        if(status === 401 || status === 403){
+            // console.log('asi');
+            await logOut();
+            navigate('/login')
+        }
+        return Promise.reject(error);
+    });
 
-//     return axiosSecure;
-// };
+    return axiosSecure;
+};
 
-// export default useAxiosSecure;
+export default useAxiosSecure;
