4b0c05a5a31aa312c576134e0e75e104ae651bd9
Thejellyfish1024 <alamin102410@gmail.com>
2024-04-12 18:29:47 +0600

    added header for request section

--- src/pages/Dashboard/AddRequests/AddRequests.jsx
+++ src/pages/Dashboard/AddRequests/AddRequests.jsx
@@ -1,14 +1,21 @@
 import SingleRequest from "./SingleRequest";
+import logo from "../../../assets/easy-chat-logo.jpg"
 
 const AddRequests = () => {
     const arr = [0, 1, 2, 4]
     return (
-        <div className="w-[510px] space-y-4 mt-4">
-            {
-                arr.map(request => <SingleRequest
-                    key={request}
-                ></SingleRequest>)
-            }
+        <div className="my-5 space-y-4 w-[510px]">
+            <div className="flex gap-2 items-center">
+                <img src={logo} className="w-10" alt="" />
+                <h3 className="text-xl  font-bold">Chat Requests</h3>
+            </div>
+            <div className="space-y-4">
+                {
+                    arr.map(request => <SingleRequest
+                        key={request}
+                    ></SingleRequest>)
+                }
+            </div>
         </div>
     );
 };
