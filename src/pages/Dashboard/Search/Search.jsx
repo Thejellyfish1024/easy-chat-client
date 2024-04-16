e8bcb40c25832207688a4b6365b0e42f7ff5d63b
Thejellyfish1024 <alamin102410@gmail.com>
2024-04-16 18:02:43 +0600

    made a small change in the search component

--- src/pages/Dashboard/Search/Search.jsx
+++ src/pages/Dashboard/Search/Search.jsx
@@ -1,11 +1,11 @@
 import { IoSearchOutline } from "react-icons/io5";
 const Search = () => {
     return (
-        <div className="flex items-center gap-2 border border-gray-500 rounded-xl py-2 px-4">
+        <div className="flex items-center gap-2 border border-gray-500 rounded-xl pl-4">
             <span>
                 <IoSearchOutline className="text-lg text-slate-500"></IoSearchOutline>
             </span>
-            <input type="text" className="w-full" 
+            <input type="text" className="w-full  rounded-r-xl pl-2 py-2" 
             placeholder="Search" />
         </div>
     );
