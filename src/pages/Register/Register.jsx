210d9f79540635e29e39db4e6aad8fac376298cc
Thejellyfish1024 <alamin102410@gmail.com>
2024-04-23 10:03:21 +0600

    made the new design for register page

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -18,7 +18,7 @@ const Register = () => {
         // console.log(user);
 
         // check user exist or not
-        const {data} = await axiosSecure.get(`/users/${user?.email}`)
+        const { data } = await axiosSecure.get(`/users/${user?.email}`)
         // console.log('is exist', data);
 
         if (!data) {
@@ -40,12 +40,12 @@ const Register = () => {
 
                 const user = {
                     name: data?.name,
-                    email : data?.email,
+                    email: data?.email,
                     image: "",
                     about: "",
                     phone: "",
-                    addRequests:[],
-                    contacts : [],
+                    addRequests: [],
+                    contacts: [],
                 };
 
                 saveUserInfoDataBase(user);
@@ -62,44 +62,44 @@ const Register = () => {
     }
 
     return (
-        <div className=' min-h-screen md:p-16 lg:p-0 lg:pt-16 bg-[#f0ead2]'>
-            <div className="flex flex-col-reverse lg:flex-row max-w-7xl mx-auto bg-[#b7e4c7] rounded-lg">
-                <div className="lg:w-3/5">
-                    <img className='rounded-l-lg h-full hidden md:block' src="https://www.froedtert.com/sites/default/files/styles/one_column/public/image/2018-09/be-an-engaged-health-care-consumer.jpg" alt="" />
+        <div className='h-screen'>
+            <div className="flex flex-col-reverse lg:flex-row h-screen rounded-lg">
+                <div className="w-[50%] relative">
+                    <img className='rounded-l-lg h-full hidden md:block object-cover' src="https://harnishdesign.net/demo/html/oxyy/images/login-bg.jpg" alt="" />
+                    <div className="absolute bg-[#0D6EFD] h-screen w-full top-0 opacity-80"></div>
                 </div>
-                <div className='lg:w-2/5 p-10'>
-                    <div className='flex gap-3 items-center justify-center'>
-                        {/* <img src={logo} className='w-20 h-20  rounded-full' alt="" /> */}
-                        <h2 className='text-3xl font-bold italic text-[#52b788]'>Elite Care</h2>
-                    </div>
-                    <div className=''>
-                        <h2 className='text-3xl font-bold my-10 text-green-600'>Register</h2>
+                <div className='w-[50%] flex justify-center items-center'>
+                    <div className='w-[65%]'>
+                        <h2 className='text-3xl font-bold mb-6'>Sign Up</h2>
 
                         {/* Register form */}
 
-                        <form onSubmit={handleSubmit(onSubmit)}>
-                            <div className='mt-4'>
-                                <h4 className='text-xl font-semibold'>Name</h4>
-                                <input type="text" {...register("name", { required: true })} name="name" placeholder='Your name' className='py-3 pl-4 w-full border border-gray-300 mt-3 rounded-md' id="" />
+                        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
+                            <div>
+                                <h4 className='font-medium'>Full Name</h4>
+                                <input type="text" {...register("name", { required: true })} name="name" placeholder='Enter your name' className='py-3 pl-4 w-full border border-gray-300 mt-3 rounded-md text-lg' id="" />
                                 {errors.name && <span className='text-red-500'>Name is required</span>}
                             </div>
-                            <div className='mt-4'>
-                                <h4 className='text-xl font-semibold'>Email</h4>
-                                <input type="email" {...register("email", { required: true })} name="email" placeholder='Email' className='py-3 pl-4 w-full border border-gray-300 mt-3 rounded-md' id="" />
+                            <div>
+                                <h4 className='font-medium'>Email Address</h4>
+                                <input type="email" {...register("email", { required: true })} name="email" placeholder='Enter your email' className='py-3 pl-4 w-full border border-gray-300 mt-3 rounded-md text-lg ' id="" />
                                 {errors.email && <span className='text-red-500'>Email is required</span>}
                             </div>
-                            <div className='mt-4'>
-                                <h4 className='text-xl font-semibold'>Password</h4>
-                                <input type="password" {...register("password", { required: true })} name="password" placeholder='Enter your password' className='py-3 pl-4 w-full border border-gray-300 mt-3 rounded-md' id="" />
+                            <div>
+                                <h4 className='font-medium'>Password</h4>
+                                <input type="password" {...register("password", { required: true })} name="Enter password" placeholder='Enter your password' className='py-3 pl-4 w-full border border-gray-300 mt-3 rounded-md text-lg' id="" />
                                 {errors.password && <span className='text-red-500'>Password is required</span>}
                             </div>
-                            <div className='text-center mt-10'>
-                                <button className='hover:bg-[#52b788] bg-green-800 w-full p-2 text-white font-bold rounded-full'>
+                            <div className='text-center'>
+                                <button className='hover: bg-[#0D6EFD] w-full py-4 text-white font-bold rounded-lg'>
                                     Register
                                 </button>
                             </div>
                         </form>
-                        <p className='text-center mt-4'>Already have an account ? <span className='text-red-600'><Link to={'/login'}>Login</Link> </span>now</p>
+                        <p className='text-center mt-4'>Already have an account ?
+                            <span className='text-[#0D6EFD]'><Link to={'/login'}> Login</Link> </span>
+                            now
+                        </p>
 
                     </div>
                 </div>
